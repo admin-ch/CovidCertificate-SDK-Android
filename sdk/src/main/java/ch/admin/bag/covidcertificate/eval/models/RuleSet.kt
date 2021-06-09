688995851e7affa4016e16f8ec411f9d9f620abd
Clea Benz <benz@ubique.ch>
2021-06-09 22:29:43 +0200

    update models

--- sdk/src/main/java/ch/admin/bag/covidcertificate/eval/models/RuleSet.kt
+++ sdk/src/main/java/ch/admin/bag/covidcertificate/eval/models/RuleSet.kt
@@ -22,7 +22,7 @@ import com.squareup.moshi.JsonClass
 data class RuleSet(
 	val rules: List<Rule>,
 	val valueSets: RuleValueSets,
-	val validDuration: Long
+	val validDuration: Long,
 )
 
 @JsonClass(generateAdapter = true)
@@ -31,7 +31,7 @@ data class Rule(
 	val businessDescription: String?,
 	val description: String,
 	val inputParameter: String,
-	@DataString val logic: String
+	@DataString val logic: String,
 )
 
 @JsonClass(generateAdapter = true)
@@ -39,26 +39,61 @@ data class RuleValueSets(
 	@Json(name = "disease-agent-targeted")
 	@get:JsonProperty("disease-agent-targeted")
 	val diseaseAgents: List<String>?,
+	@Json(name = "accepted-vaccines")
+	@get:JsonProperty("accepted-vaccines")
+	val acceptedVaccines: List<String>?,
+	@Json(name = "two-dose-vaccines")
+	@get:JsonProperty("two-dose-vaccines")
+	val twoDoseVaccines: List<String>?,
+	@Json(name = "one-dose-vaccines-with-offset")
+	@get:JsonProperty("one-dose-vaccines-with-offset")
+	val oneDoseVaccinesWithOffset: List<String>?,
 	@Json(name = "covid-19-lab-test-type")
 	@get:JsonProperty("covid-19-lab-test-type")
 	val testTypes: List<String>?,
 	@Json(name = "covid-19-lab-test-manufacturer-and-name")
 	@get:JsonProperty("covid-19-lab-test-manufacturer-and-name")
-	val testManufacturers: List<String>?
+	val testManufacturers: List<String>?,
+	@Json(name = "acceptance-criteria")
+	@get:JsonProperty("acceptance-criteria")
+	val acceptanceCriteria: AcceptanceCriterias,
 )
+
+@JsonClass(generateAdapter = true)
+data class AcceptanceCriterias(
+	@Json(name = "single-vaccine-validity-offset")
+	@get:JsonProperty("single-vaccine-validity-offset")
+	val singleVaccineValidityOffset: Int,
+	@Json(name = "vaccine-immunity")
+	@get:JsonProperty("vaccine-immunity")
+	val vaccineImmunity: Int,
+	@Json(name = "rat-test-validity")
+	@get:JsonProperty("rat-test-validity")
+	val ratTestValidity: Int,
+	@Json(name = "pcr-test-validity")
+	@get:JsonProperty("pcr-test-validity")
+	val pcrTestValidity: Int,
+	@Json(name = "recovery-offset-valid-from")
+	@get:JsonProperty("recovery-offset-valid-from")
+	val recoveryOffsetValidFrom: Int,
+	@Json(name = "recovery-offset-valid-until")
+	@get:JsonProperty("recovery-offset-valid-until")
+	val recoveryOffsetValidUntil: Int
+)
+
 
 data class CertLogicData(
 	val payload: CertLogicPayload,
-	val external: CertLogicExternalInfo
+	val external: CertLogicExternalInfo,
 )
 
 data class CertLogicPayload(
 	val r: List<RecoveryEntry>? = null,
 	val t: List<TestEntry>? = null,
-	val v: List<VaccinationEntry>? = null
+	val v: List<VaccinationEntry>? = null,
 )
 
 data class CertLogicExternalInfo(
 	val valueSets: RuleValueSets,
-	val validationClock: String // ISO-8601 extended offset date-time format
+	val validationClock: String, // ISO-8601 extended offset date-time format
 )
